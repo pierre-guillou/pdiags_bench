dc34fae5972db62efca94394011a0c94b060ce55
Pierre Guillou <pierre.guillou@lip6.fr>
2020-11-03 16:05:28 +0100

    Convert RAW files to VTI

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import glob
 import json
 import math
 
 import requests
+from paraview import simple
 
 URL = "https://klacansky.com/open-scivis-datasets/data_sets.json"
 SIZE_LIMIT = 128 ** 3
@@ -32,9 +34,40 @@ def download_datasets(datasets_urls):
         download_dataset(url)
 
 
+def convert_to_vti(raw_file):
+    extent, dtype = raw_file.split("_")[-2:]
+    extent = [int(dim) for dim in extent.split("x")]
+
+    def get_dtype(dtype):
+        if dtype == "uint8":
+            return "unsigned char"
+        elif dtype == "int16":
+            return "signed short"
+        elif dtype == "uint16":
+            return "unsigned short"
+        elif dtype == "float32":
+            return "float"
+        elif dtype == "float64":
+            return "double"
+
+    dtype = get_dtype(dtype.split(".")[0])
+
+    raw = simple.ImageReader(FileNames=[raw_file])
+    raw.DataScalarType = dtype
+    raw.DataExtent = [0, extent[0] - 1, 0, extent[1] - 1, 0, extent[2] - 1]
+    simple.SaveData(
+        raw_file.split(".")[0] + ".vti",
+        proxy=raw,
+        PointDataArrays=["ImageFile"],
+    )
+    print("Converted " + raw_file + " to VTI")
+
+
 def main():
-    datasets_urls = get_datasets_urls()
-    download_datasets(datasets_urls)
+    # datasets_urls = get_datasets_urls()
+    # download_datasets(datasets_urls)
+    for dataset in glob.glob("*.raw"):
+        convert_to_vti(dataset)
 
 
 if __name__ == "__main__":
