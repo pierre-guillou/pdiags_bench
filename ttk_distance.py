d0cf345c99527737439277c9db4b7020a46b67a7
Pierre Guillou <pierre.guillou@lip6.fr>
2020-11-17 14:44:17 +0100

    Threshold before bottleneck

--- ttk_distance.py
+++ ttk_distance.py
@@ -19,9 +19,17 @@ diag1 = load_diagram(sys.argv[3])
 
 if sys.argv[1] == "bottleneck":
 
+    thr0 = simple.Threshold(Input=diag0)
+    thr0.Scalars = ["CELLS", "Persistence"]
+    thr0.ThresholdRange = [0.1, 1.0]
+
+    thr1 = simple.Threshold(Input=diag1)
+    thr1.Scalars = ["CELLS", "Persistence"]
+    thr1.ThresholdRange = [0.1, 1.0]
+
     dist = simple.TTKBottleneckDistance(
-        Persistencediagram1=diag0,
-        Persistencediagram2=diag1,
+        Persistencediagram1=thr0,
+        Persistencediagram2=thr1,
     )
 
     save = simple.SaveData("dist.vtu", Input=dist)
