f6fdf8fa0b707937a72d55c7d32f06a885693522
Pierre Guillou <pierre.guillou@lip6.fr>
2020-11-17 14:45:00 +0100

    Factor threshold lower bound

--- ttk_distance.py
+++ ttk_distance.py
@@ -16,16 +16,18 @@ def load_diagram(diag):
 
 diag0 = load_diagram(sys.argv[2])
 diag1 = load_diagram(sys.argv[3])
+thr_bound = 0.1
+
 
 if sys.argv[1] == "bottleneck":
 
     thr0 = simple.Threshold(Input=diag0)
     thr0.Scalars = ["CELLS", "Persistence"]
-    thr0.ThresholdRange = [0.1, 1.0]
+    thr0.ThresholdRange = [thr_bound, 1.0]
 
     thr1 = simple.Threshold(Input=diag1)
     thr1.Scalars = ["CELLS", "Persistence"]
-    thr1.ThresholdRange = [0.1, 1.0]
+    thr1.ThresholdRange = [thr_bound, 1.0]
 
     dist = simple.TTKBottleneckDistance(
         Persistencediagram1=thr0,
@@ -39,7 +41,7 @@ elif sys.argv[1] == "auction":
     gd = simple.GroupDatasets(Input=[diag0, diag1])
     thr = simple.Threshold(Input=gd)
     thr.Scalars = ["CELLS", "Persistence"]
-    thr.ThresholdRange = [0.1, 1.0]
+    thr.ThresholdRange = [thr_bound, 1.0]
     dist = simple.TTKPersistenceDiagramClustering(Input=thr)
     dist.Maximalcomputationtimes = 10.0
     pairTypes = ["min-saddle pairs", "saddle-saddle pairs", "saddle-max pairs"]
