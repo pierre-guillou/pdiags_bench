ff9e07d26135463fdd927b00d7f0724a269f85e9
Pierre Guillou <pierre.guillou@lip6.fr>
2021-01-13 14:11:16 +0100

    Also generate elevation fields

--- gen_random.py
+++ gen_random.py
@@ -6,13 +6,24 @@ fug = simple.FastUniformGrid()
 ext = int(sys.argv[1]) if len(sys.argv) > 0 else 8
 fug.WholeExtent = [0, ext - 1, 0, ext - 1, 0, ext - 1]
 
-rattr = simple.RandomAttributes(Input=fug)
-rattr.DataType = "Float"
-rattr.ComponentRange = [0.0, 1.0]
-rattr.GeneratePointScalars = 1
-rattr.GenerateCellVectors = 0
+if len(sys.argv) > 2 and sys.argv[2] == "elev":
+    elev = simple.Elevation(Input=fug)
+    elev.LowPoint = [0, 0, 0]
+    elev.HighPoint = [ext - 1, ext - 1, ext - 1]
 
-pa = simple.PassArrays(Input=rattr)
-pa.PointDataArrays = ["RandomPointScalars"]
+    pa = simple.PassArrays(Input=elev)
+    pa.PointDataArrays = ["Elevation"]
 
-simple.SaveData("random.vti", Input=pa)
+    simple.SaveData("elevation.vti", Input=pa)
+
+else:
+    rattr = simple.RandomAttributes(Input=fug)
+    rattr.DataType = "Float"
+    rattr.ComponentRange = [0.0, 1.0]
+    rattr.GeneratePointScalars = 1
+    rattr.GenerateCellVectors = 0
+
+    pa = simple.PassArrays(Input=rattr)
+    pa.PointDataArrays = ["RandomPointScalars"]
+
+    simple.SaveData("random.vti", Input=pa)
