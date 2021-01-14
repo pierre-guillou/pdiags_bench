817d9903bc3f731d4cd692fe75c0e71bb56714a6
Pierre Guillou <pierre.guillou@lip6.fr>
2021-01-14 19:26:57 +0100

    Add test_random script

---
+++ test_random.py
@@ -0,0 +1,48 @@
+from paraview import simple
+
+import compare_diags
+import gen_random
+import main as compute_diags
+
+
+def generate_explicit(inp, out):
+    # read random.vti
+    rand = simple.XMLImageDataReader(FileName=inp)
+    # compute order field
+    arrprec = simple.TTKArrayPreconditioning(Input=rand)
+    arrprec.PointDataArrays = ["RandomPointScalars"]
+    # convert order field to float
+    pdc2 = simple.TTKPointDataConverter(Input=arrprec)
+    pdc2.PointDataScalarField = ["POINTS", "RandomPointScalars_Order"]
+    pdc2.OutputType = "Float"
+    # normalize order field
+    sfnorm2 = simple.TTKScalarFieldNormalizer(Input=pdc2)
+    sfnorm2.ScalarField = ["POINTS", "RandomPointScalars_Order"]
+    # trash input scalar field, save order field
+    pa = simple.PassArrays(Input=sfnorm2)
+    pa.PointDataArrays = ["RandomPointScalars_Order"]
+    # tetrahedralize grid
+    tetrah = simple.Tetrahedralize(Input=pa)
+
+    # vtkUnstructuredGrid (TTK)
+    simple.SaveData(out + ".vtu", proxy=tetrah)
+    # Dipha Explicit Complex (Dipha)
+    simple.SaveData(out + ".dipha", proxy=tetrah)
+
+
+def main():
+    fname = "random_order_sfnorm_expl"
+    for i in range(8, 50):
+        gen_random.main(i, "rand")
+        ds = "datasets/" + fname
+        generate_explicit("random.vti", ds)
+        tm = dict()
+        tm[fname.split("/")[-1]] = dict()
+        compute_diags.compute_ttk(ds + ".vtu", "ttkPersistenceDiagramCmd", tm)
+        compute_diags.compute_dipha(ds + ".dipha", "build_dipha/dipha", tm)
+        diag = "diagrams/" + fname
+        compare_diags.main(diag + ".vtu", diag + ".dipha")
+
+
+if __name__ == "__main__":
+    main()
