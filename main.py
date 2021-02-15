0564e3bbda4d233a9d7c95dd6aead8062854f0f5
Pierre Guillou <pierre.guillou@lip6.fr>
2021-02-15 14:46:16 +0100

    [main] Remove network ops to not depend on requests

--- main.py
+++ main.py
@@ -3,53 +3,15 @@
 import argparse
 import glob
 import json
-import math
 import os
 import re
 import subprocess
-import tarfile
 import time
 
-import requests
 from paraview import simple
 
 URL = "https://klacansky.com/open-scivis-datasets/data_sets.json"
 SIZE_LIMIT_MB = 80
-
-
-def get_datasets_urls(size_limit_mb):
-    req = requests.get(URL)
-    datasets_json = json.loads(req.text)
-
-    dtype_size = {
-        "uint8": 1,
-        "int16": 2,
-        "uint16": 2,
-        "float32": 4,
-        "float64": 8,
-    }
-
-    return [
-        dataset["url"]
-        for dataset in datasets_json
-        if math.prod(dataset["size"]) * dtype_size[dataset["type"]]
-        < (size_limit_mb * 1e6)
-    ]
-
-
-def download_dataset(dataset_url, name=None):
-    dataset_name = name if name is not None else dataset_url.split("/")[-1]
-    # https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
-    with requests.get(dataset_url, stream=True) as req:
-        with open(dataset_name, "wb") as dest:
-            for chunk in req.iter_content(chunk_size=8192):
-                dest.write(chunk)
-    print("Downloaded " + dataset_name)
-
-
-def download_datasets(datasets_urls):
-    for url in datasets_urls:
-        download_dataset(url)
 
 
 def create_dir(dirname):
@@ -123,39 +85,8 @@ def convert_dataset(raw_file):
 
 
 def prepare_datasets(_, size_limit=SIZE_LIMIT_MB, download=False):
-    if download:
-        datasets_urls = get_datasets_urls(size_limit)
-        download_datasets(datasets_urls)
     for dataset in sorted(glob.glob("*.raw")):
         convert_dataset(dataset.split("/")[-1])
-
-
-def download_and_build_software(_):
-    gh = "https://github.com"
-    tb = "tarball"
-    gudhi_url = f"{gh}/GUDHI/gudhi-devel/{tb}/tags%2Fgudhi-release-3.3.0"
-    CubicalRipser_url = f"{gh}/CubicalRipser/CubicalRipser_3dim/{tb}/master"
-    dipha_url = f"{gh}/DIPHA/dipha/{tb}/master"
-
-    softs = ["dipha", "gudhi", "CubicalRipser"]
-
-    # download and extract a tarball from GitHub
-    for soft in softs:
-        download_dataset(locals()[soft + "_url"], soft + ".tar.gz")
-        with tarfile.open(soft + ".tar.gz", "r:gz") as src:
-            src.extractall()
-            # rename software folders
-            os.rename(src.getmembers()[0].name, soft)
-        print("Extracted " + soft + " archive")
-
-    # build the 3 applications
-    subprocess.check_call(["make", "-C", "CubicalRipser"])
-
-    for cmake_soft in ["dipha", "gudhi"]:
-        builddir = "build_" + cmake_soft
-        create_dir(builddir)
-        subprocess.check_call("cmake", "-S", cmake_soft, "-B", builddir)
-        subprocess.check_call("cmake", "--build", builddir)
 
 
 def dipha_print_pairs(dipha_diag):
@@ -393,9 +324,6 @@ def main():
 
     prep_datasets = subparsers.add_parser("prepare_datasets")
     prep_datasets.set_defaults(func=prepare_datasets)
-
-    dl_softs = subparsers.add_parser("download_software")
-    dl_softs.set_defaults(func=download_and_build_software)
 
     get_diags = subparsers.add_parser("compute_diagrams")
     get_diags.set_defaults(func=compute_diagrams)
