2516b21e9fa24e442e00a5ed13f5a839c6c4333d
Pierre Guillou <pierre.guillou@lip6.fr>
2021-02-18 15:19:51 +0100

    Working Dipha launch script for MeSU

--- pbs.sh
+++ pbs.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #PBS -S /bin/bash
-#PBS -q alpha
-#PBS -l select=1:ncpus=64
+#PBS -q beta
+#PBS -l select=1:ncpus=24:mpiprocs=24
 #PBS -l walltime=00:05:00
 #PBS -N dipha_bench
+#PBS -j oe
 
 #load appropriate modules
 module purge
-module load gcc/8.2 openMPI/4.0.3
+module load mpt/2.18
 
 #move to PBS_O_WORKDIR
 cd $PBS_O_WORKDIR
 
 # Define scratch space
 PROJECT=dipha
-SCRATCH=/scratchalpha/$USER/$PROJECT
+SCRATCH=/scratchbeta/$USER/$PROJECT
 mkdir -p $SCRATCH
 
 # working directory
@@ -27,7 +28,7 @@ cp $WD/datasets/$DS  $SCRATCH
 
 #execute your program
 cd $SCRATCH || exit 1
-mpirun -np 64 \
+mpirun \
        $WD/build_dipha/dipha \
        --benchmark \
        --upper_dim 3 \
