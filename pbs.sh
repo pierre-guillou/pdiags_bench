4140f9e380ec681805e0cf4562b3460d06e5de27
Pierre Guillou <pierre.guillou@lip6.fr>
2021-02-22 16:06:49 +0100

    [pbs] Disable Dipha benchmark mode

--- pbs.sh
+++ pbs.sh
@@ -57,7 +57,7 @@ for raw in raws/*.raw; do
     dipha=${vtu%.vtu}.dipha
     echo "Processing $dipha with Dipha..." >> $out
     mpirun -np $NCPUS --oversubscribe \
-           dipha --benchmark --upper_dim 3 $dipha output.dipha \
+           dipha --upper_dim 3 $dipha output.dipha \
            1>> $out 2>> $err
     rm $dipha $vtu
 done
