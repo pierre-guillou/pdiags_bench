94adf9293855c958e83558cfa270159fe6a41a9f
Pierre Guillou <pierre.guillou@lip6.fr>
2021-04-30 14:06:12 +0200

    [dionysus_gudhi_persistence] Also support ripser

--- dionysus_gudhi_persistence.py
+++ dionysus_gudhi_persistence.py
@@ -1,8 +1,11 @@
+import math
 import time
 
 import dionysus
 import gudhi
 import numpy as np
+import ripser
+import scipy.sparse
 
 
 def read_simplicial_complex(dataset):
@@ -12,7 +15,7 @@ def read_simplicial_complex(dataset):
         magic = src.read(20)
         if magic != b"TTKSimplicialComplex":
             print("Not a TTK Simplicial Complex file")
-            return None
+            raise TypeError
 
         ncells = int.from_bytes(src.read(4), "little", signed=True)
         print(f"Number of cells: {ncells}")
@@ -39,11 +42,43 @@ def read_simplicial_complex(dataset):
     return dims, values, (edges, triangles, tetras)
 
 
+class Ripser_SparseDM:
+    def __init__(self):
+        self.dist_mat = None
+        self.diag = None
+        print("Using the Ripser.py backend")
+
+    def fill_dist_mat(self, dims, vals, edges):
+        edges = edges.reshape(-1, 2)
+        I = np.zeros(dims[1], dtype=np.int32)
+        J = np.zeros(dims[1], dtype=np.int32)
+        V = np.zeros(dims[1], dtype=np.double)
+
+        for i, e in enumerate(edges):
+            I[i] = e[0]
+            J[i] = e[1]
+            V[i] = vals[dims[0] + i]
+
+        self.dist_mat = scipy.sparse.coo_matrix((V, (I, J)), shape=(dims[0], dims[0]))
+
+    def compute_pers(self):
+        self.diag = ripser.ripser(self.dist_mat, distance_matrix=True, maxdim=2)["dgms"]
+
+    def write_diag(self, output):
+        with open(output, "w") as dst:
+            for i, pairs in enumerate(self.diag):
+                for pair in pairs:
+                    if i == 0 and pair[0] == 0 and pair[1] != math.inf:
+                        # filter out min-sad pairs beginning from 0
+                        continue
+                    dst.write(f"{i} {pair[0]} {pair[1]}\n")
+
+
 class Dionysus_Filtration:
     def __init__(self):
         self.f = dionysus.Filtration()
         self.diag = None
-        print("Use the Dionysus2 backend")
+        print("Using the Dionysus2 backend")
 
     def add(self, verts, val):
         self.f.append(dionysus.Simplex(verts, val))
@@ -63,7 +98,7 @@ class Dionysus_Filtration:
 class Gudhi_SimplexTree:
     def __init__(self):
         self.st = gudhi.SimplexTree()
-        print("Use the Gudhi Simplex Tree backend")
+        print("Using the Gudhi Simplex Tree backend")
 
     def add(self, verts, val):
         self.st.insert(verts, filtration=val)
@@ -80,23 +115,26 @@ def compute_persistence(wrapper, dims, values, cpx, output):
 
     edges, triangles, tetras = cpx
 
-    for i in range(dims[0]):
-        wrapper.add([i], values[i])
-    for i in range(dims[1]):
-        o = 2 * i
-        a = dims[0] + i
-        wrapper.add(edges[o : o + 2], values[a])
-    for i in range(dims[2]):
-        o = 3 * i
-        a = dims[0] + dims[1] + i
-        wrapper.add(triangles[o : o + 3], values[a])
-    for i in range(dims[3]):
-        o = 4 * i
-        a = dims[0] + dims[1] + dims[2] + i
-        wrapper.add(tetras[o : o + 4], values[a])
+    if isinstance(wrapper, Ripser_SparseDM):
+        wrapper.fill_dist_mat(dims, values, edges)
+    else:
+        for i in range(dims[0]):
+            wrapper.add([i], values[i])
+        for i in range(dims[1]):
+            o = 2 * i
+            a = dims[0] + i
+            wrapper.add(edges[o : o + 2], values[a])
+        for i in range(dims[2]):
+            o = 3 * i
+            a = dims[0] + dims[1] + i
+            wrapper.add(triangles[o : o + 3], values[a])
+        for i in range(dims[3]):
+            o = 4 * i
+            a = dims[0] + dims[1] + dims[2] + i
+            wrapper.add(tetras[o : o + 4], values[a])
 
     prec = round(time.time() - start, 3)
-    print(f"Filled filtration/simplex tree: {prec}s")
+    print(f"Filled filtration/simplex tree/distance matrix: {prec}s")
     start = time.time()
 
     wrapper.compute_pers()
@@ -115,6 +153,7 @@ def main(dataset, output, backend="Gudhi", simplicial=True):
         dispatch = {
             "Dionysus": Dionysus_Filtration,
             "Gudhi": Gudhi_SimplexTree,
+            "Ripser": Ripser_SparseDM,
         }
         return compute_persistence(dispatch[backend](), dims, vals, cpx, output)
 
