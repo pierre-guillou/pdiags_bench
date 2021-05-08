5a8617a83506699e2846858c68427c2d6958d8d6
Pierre Guillou <pierre.guillou@lip6.fr>
2021-05-08 15:28:07 +0200

    [test_ripser] Use C++ binary

--- test_ripser.py
+++ test_ripser.py
@@ -1,68 +1,34 @@
-import time
+import math
+import re
+import subprocess
 
 import numpy as np
-import ripser
-from scipy import sparse
 
 import dionysus_gudhi_persistence
 
 
-def lower_star_img(img):
-    """
-    Construct a lower star filtration on an image
-    Parameters
-    ----------
-    img: ndarray (M, N)
-        An array of single channel image data
-    Returns
-    -------
-    I: ndarray (K, 2)
-        A 0-dimensional persistence diagram corresponding to the sublevelset filtration
-    """
-    m, n = img.shape
+def parse_write_pairs(ripser_pairs, output):
+    dim = 0
+    pairs = list()
+    for line in ripser_pairs.split("\n"):
+        dim_pat = r"persistence intervals in dim (\d+):"
+        try:
+            dim = re.search(dim_pat, line).group(1)
+        except AttributeError:
+            pass
+        pair_pat = r"\[(\d+),(\d+)\)"
+        try:
+            birth, death = re.search(pair_pat, line).groups()
+            pairs.append([dim, birth, death])
+        except AttributeError:
+            pass
 
-    idxs = np.arange(m * n).reshape((m, n))
-
-    I = idxs.flatten()
-    J = idxs.flatten()
-    V = img.flatten()
-
-    # Connect 8 spatial neighbors
-    tidxs = np.ones((m + 2, n + 2), dtype=np.int64) * np.nan
-    tidxs[1:-1, 1:-1] = idxs
-
-    tD = np.ones_like(tidxs) * np.nan
-    tD[1:-1, 1:-1] = img
-
-    for di in [-1, 0, 1]:
-        for dj in [-1, 0, 1]:
-
-            if di == 0 and dj == 0:
+    with open(output, "w") as dst:
+        for dim, birth, death in pairs:
+            if int(dim) == 0 and float(birth) == 0 and float(death) != math.inf:
+                # filter out min-sad pairs beginning from 0
                 continue
-
-            thisJ = np.roll(np.roll(tidxs, di, axis=0), dj, axis=1)
-            thisD = np.roll(np.roll(tD, di, axis=0), dj, axis=1)
-            thisD = np.maximum(thisD, tD)
-
-            # Deal with boundaries
-            boundary = ~np.isnan(thisD)
-            thisI = tidxs[boundary]
-            thisJ = thisJ[boundary]
-            thisD = thisD[boundary]
-
-            I = np.concatenate((I, thisI.flatten()))
-            J = np.concatenate((J, thisJ.flatten()))
-            V = np.concatenate((V, thisD.flatten()))
-
-    sparseDM = sparse.coo_matrix((V, (I, J)), shape=(idxs.size, idxs.size))
-
-    print(sparseDM.todense())
-
-
-def test():
-    data = np.arange(0, 9).reshape((3, 3))
-    print(data)
-    lower_star_img(data)
+            dst.write(f"{dim} {birth} {death}\n")
 
 
 def main(dataset, output):
@@ -70,8 +36,6 @@ def main(dataset, output):
         dataset
     )
     edges = edges.reshape(-1, 2)
-
-    start = time.time()
 
     I = np.zeros(dims[1], dtype=np.int32)
     J = np.zeros(dims[1], dtype=np.int32)
@@ -82,21 +46,16 @@ def main(dataset, output):
         J[i] = e[1]
         V[i] = vals[dims[0] + i]
 
-    sparseDM = sparse.coo_matrix((V, (I, J)), shape=(dims[0], dims[0]))
+    sparse_mat = "\n".join([f"{i} {j} {v}" for i, j, v in zip(I, J, V)]).encode()
 
-    print(f"Filled sparse distance matrix: {time.time() - start:.3f}s")
-    start = time.time()
-
-    diag = ripser.ripser(sparseDM, distance_matrix=True, maxdim=2)["dgms"]
-
-    print(f"Computed persistence diagram: {time.time() - start:.3f}s")
-
-    with open(output, "w") as dst:
-        for i, pairs in enumerate(diag):
-            for pair in pairs:
-                dst.write(f"{i} {pair[0]} {pair[1]}\n")
+    proc = subprocess.run(
+        ["ripser/ripser", "--format", "sparse", "--dim", "2"],
+        check=True,
+        input=sparse_mat,
+        capture_output=True,
+    )
+    parse_write_pairs(proc.stdout.decode(), output)
 
 
 if __name__ == "__main__":
-    test()
     main("datasets/fuel_64x64x64_uint8_order_expl.tsc", "out.gudhi")
