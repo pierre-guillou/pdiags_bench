7be8c573d2599fe0eb8a0cd505318e7714ee4336
Pierre Guillou <pierre.guillou@lip6.fr>
2021-05-11 19:58:37 +0200

    [test_ripser] Test from raw

--- test_ripser.py
+++ test_ripser.py
@@ -1,10 +1,66 @@
+import argparse
 import math
 import re
 import subprocess
 
 import numpy as np
 
-import dionysus_gudhi_persistence
+
+def load_raw(input_raw):
+    extension = input_raw.split(".")[-1]
+    if extension != "raw":
+        print("Need a .raw file")
+        raise TypeError
+
+    extent, dtype = input_raw.split(".")[0].split("_")[-2:]
+    extent = [int(dim) for dim in extent.split("x")]
+
+    dtype_np = {
+        "uint8": np.uint8,
+        "int16": np.int16,
+        "uint16": np.uint16,
+        "float32": np.float32,
+        "float64": np.float64,
+    }
+    dtype = dtype_np[dtype]
+
+    with open(input_raw) as src:
+        data = np.fromfile(src, dtype=dtype)
+        return data.reshape(extent)
+
+
+def compute_persistence(sparse_triplets):
+    proc = subprocess.Popen(
+        ["ripser/ripser", "--format", "sparse", "--dim", "2"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    out, _ = proc.communicate(sparse_triplets)
+    return out
+
+
+def build_sparse_triplets(data):
+    triplets = list()
+
+    with np.nditer(data, flags=["multi_index"]) as it:
+        for v in it:
+            for d in range(data.ndim):
+                for s in [-1, 1]:
+                    coords = list(it.multi_index)
+                    s = coords[d] + s
+                    if s < 0 or s >= data.shape[d]:
+                        continue
+                    coords[d] = s
+                    triplets.append(
+                        [
+                            np.ravel_multi_index(it.multi_index, data.shape),
+                            np.ravel_multi_index(coords, data.shape),
+                            max(data[tuple(coords)], v),
+                        ]
+                    )
+
+    return "\n".join([f"{i} {j} {v}" for i, j, v in triplets])
 
 
 def parse_write_pairs(ripser_pairs, output):
@@ -31,31 +87,22 @@ def parse_write_pairs(ripser_pairs, output):
             dst.write(f"{dim} {birth} {death}\n")
 
 
-def main(dataset, output):
-    dims, vals, (edges, _, _) = dionysus_gudhi_persistence.read_simplicial_complex(
-        dataset
-    )
-    edges = edges.reshape(-1, 2)
-
-    I = np.zeros(dims[1], dtype=np.int32)
-    J = np.zeros(dims[1], dtype=np.int32)
-    V = np.zeros(dims[1], dtype=np.double)
-
-    for i, e in enumerate(edges):
-        I[i] = e[0]
-        J[i] = e[1]
-        V[i] = vals[dims[0] + i]
-
-    sparse_mat = "\n".join([f"{i} {j} {v}" for i, j, v in zip(I, J, V)]).encode()
-
-    proc = subprocess.run(
-        ["ripser/ripser", "--format", "sparse", "--dim", "2"],
-        check=True,
-        input=sparse_mat,
-        capture_output=True,
-    )
-    parse_write_pairs(proc.stdout.decode(), output)
+def main(input_raw, output_diagram):
+    data = load_raw(input_raw)
+    sparse_triplets = build_sparse_triplets(data)
+    ripser_pairs = compute_persistence(sparse_triplets)
+    parse_write_pairs(ripser_pairs, output_diagram)
 
 
 if __name__ == "__main__":
-    main("datasets/fuel_64x64x64_uint8_order_expl.tsc", "out.gudhi")
+    parser = argparse.ArgumentParser(description="Wrapper around Ripser C++")
+    parser.add_argument("input_raw", help="Input raw file from OpenSciVis datasets")
+    parser.add_argument(
+        "-o",
+        "--output_diagram",
+        help="Output diagram in Gudhi format",
+        default="out.gudhi",
+    )
+    args = parser.parse_args()
+
+    main(args.input_raw, args.output_diagram)
