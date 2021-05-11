024d3b22a28e487024d8af86ed48f0eb09d741f3
Pierre Guillou <pierre.guillou@lip6.fr>
2021-05-11 14:43:51 +0200

    [eirene] Write diag to file, handle CLI arguments

--- test_eirene.jl
+++ test_eirene.jl
@@ -1,5 +1,23 @@
 using Eirene
+using Printf
 
-C = eirene("eirene.csv",model="complex",entryformat="ev")
-plotbarcode_pjs(C,dim=1)
-plotpersistencediagram_pjs(C,dim=1)
+input_csv = "eirene.csv"
+output_diag = "diag_eirene.gudhi"
+
+if size(ARGS, 1) >= 1
+    output_diag = ARGS[2]
+end
+if size(ARGS, 1) >= 2
+    input_csv = ARGS[1]
+end
+
+@time C = eirene(input_csv, model="complex", entryformat="ev", maxdim=2)
+
+open(output_diag, "w") do io
+    for d in 0:2
+        A = barcode(C, dim=d)
+        for i = 1:size(A, 1)
+            println(io, @sprintf "%d %.3f %.3f" d A[i, 1] A[i, 2])
+        end
+    end
+end
