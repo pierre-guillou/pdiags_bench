cbfbe748a409dfa8b2a5bfd841128478a0abba35
Pierre Guillou <pierre.guillou@lip6.fr>
2021-06-30 15:32:36 +0200

    [gudhi_diag_inf] Skip rewriting file when not necessary

--- gudhi_diag_inf.py
+++ gudhi_diag_inf.py
@@ -10,12 +10,18 @@ def replace_inf(diag):
     dims = [int(d) for d in dims]
     max_order = dims[0] * dims[1] * dims[2]
     out_pairs = list()
+    found = False
     for line in pairs.split("\n"):
         if "inf" in line:
             line = line.replace("inf", str(max_order))
+            found = True
         out_pairs.append(line)
+    if not found:
+        # skip rewrite part if no "inf" in file
+        return found
     with open(diag, "w") as dst:
         dst.write("\n".join(out_pairs))
+    return found
 
 
 def main():
@@ -25,8 +31,11 @@ def main():
     """
     p = pathlib.Path("diagrams")
     for diag in sorted(p.glob("*.gudhi")):
-        replace_inf(diag)
-        print(f"Post-processed {diag}")
+        found = replace_inf(diag)
+        if found:
+            print(f"Post-processed {diag}")
+        else:
+            print(f"Skipped {diag}")
 
 
 if __name__ == "__main__":
