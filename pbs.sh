74d433a672949b158dc0bd67924b02eaeb88326c
Pierre Guillou <pierre.guillou@lip6.fr>
2022-01-12 14:21:01 +0100

    Permute loops

--- pbs.sh
+++ pbs.sh
@@ -36,34 +36,30 @@ cd $SCRATCH || exit 1
 mkdir datasets
 
 for raw in raws/*.raw; do
-    echo "Converting $raw..."
-    time python3 $WD/convert_datasets.py -d datasets $raw
-done
+    raw_stem=${raw#raws/}
+    out=$WD/log/${raw_stem}_${PBS_JOBID}.out
+    err=$WD/log/${raw_stem}_${PBS_JOBID}.err
 
-for vtu in datasets/*.vtu; do
-    vtu_stem=${vtu#datasets/}
-    out=$WD/log/${vtu_stem}_${PBS_JOBID}_${NCPUS}.out
-    err=$WD/log/${vtu_stem}_${PBS_JOBID}_${NCPUS}.err
-    for nt in 1 32 64 96 128; do
-        echo "Processing $vtu with TTK with $nt threads..." >> $out
-        omplace -nt $nt \
-                ttkPersistenceDiagramCmd -B 2 -d 4 -i $vtu -t $nt \
-                1>> $out 2>> $err
-    done
-    rm $vtu
-done
+    echo "Converting $raw..." 1> $out 2> $err
+    time python3 $WD/convert_datasets.py -d datasets $raw 1> $out 2> $err
 
-for dph in datasets/*.dipha; do
-    dph_stem=${dph#datasets/}
-    out=$WD/log/${dph_stem}_${PBS_JOBID}_${NCPUS}.out
-    err=$WD/log/${dph_stem}_${PBS_JOBID}_${NCPUS}.err
-    for np in 1 32 64 96 128; do
-        echo "Processing $dph with Dipha with $np processes..." >> $out
-        mpirun -np $np --oversubscribe \
-               dipha --benchmark $dph out.dipha \
-               1>> $out 2>> $err
+    for nt in 1 32 64 128; do
+        for vtu in datasets/*.vtu; do
+            echo "Processing $vtu with TTK with $nt threads..." >> $out
+            time omplace -nt $nt \
+                 ttkPersistenceDiagramCmd -B 2 -d 4 -i $vtu -t $nt \
+                 1>> $out 2>> $err
+        done
+
+        for dph in datasets/*.dipha; do
+            echo "Processing $dph with Dipha with $nt processes..." >> $out
+            mpirun -np $nt --oversubscribe \
+                 dipha --benchmark $dph out.dipha \
+                 1>> $out 2>> $err
+        done
     done
-    rm $dph
+
+    rm datasets/*
 done
 
 # copy some output files to submission directory
